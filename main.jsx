4d7ec105d486151a180f445b351f15aa9aa044fe
Java-Linda <58697548+Java-Linda@users.noreply.github.com>
2023-08-14 17:10:57 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import accountReducer from './redux/accountSlice';
+import App from './App';
+
+// Create the Redux store
+const store = configureStore({
+   // ***TODO***
+  
+   // Set the accountReducer as the reducer for the 'account' state
+   reducer: {
+      account: accountReducer
+   }
+  
+});
+
+// Render the App component inside the Provider component
+ReactDOM.createRoot(document.getElementById("root")).render(
+   // ***TODO***
+   <React.StrictMode>
+      <Provider store = {store}>
+         <App />
+      </Provider>
+   </React.StrictMode>
+  
+);
